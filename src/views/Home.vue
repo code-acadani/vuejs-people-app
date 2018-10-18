8df34bf19f39407703f7ecd2a5ba7a14dfd424d2
danizaghian <dani.zaghian@gmail.com>
2018-10-18 09:38:52 -0700

    adds fat arrow functions and error handling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,10 @@
       <button v-on:click="addPerson()">Add Person</button>
     </div>
 
+    <div>
+      <li v-for="error in errors">{{ error }}</li>
+    </div>
+
     <div v-for="person in people">
       <h2 v-on:click="toggleBioVisible(person)">{{ person.name }}</h2> 
       <h3 v-bind:class="{strike: !person.bioVisible}">{{ person.bio }}</h3>
@@ -30,14 +34,15 @@ export default {
   data: function() {
     return {
       people: [],
-      newPerson: {name: "", bio: "", bioVisible: true}
+      newPerson: {name: "", bio: ""},
+      errors: []
     };
   },
   created: function() {
-    axios.get("http://localhost:3000/api/people").then(function(response){
+    axios.get("http://localhost:3000/api/people").then(response => {
       console.log(response.data);
       this.people = response.data;
-    }.bind(this));
+    });
   },
   methods: {
     addPerson: function() {
@@ -45,18 +50,20 @@ export default {
         name: this.newPerson.name, 
         bio: this.newPerson.bio
       }
-      axios.post("http://localhost:3000/api/people", params).then(function(response){
+      axios.post("http://localhost:3000/api/people", params).then(response => {
         this.people.push(response.data);
         this.newPerson.name = "";
         this.newPerson.bio = "";
-      }.bind(this));
+      }).catch(error => {
+        this.errors = error.response.data.errors;
+      });
     },
     deletePerson: function(person) {
-      axios.delete("http://localhost:3000/api/people/" + person.id).then(function(response){
+      axios.delete("http://localhost:3000/api/people/" + person.id).then(response => {
         // find the index of this person in the people array
         var index = this.people.indexOf(person);
         this.people.splice(index, 1);
-      }.bind(this));
+      });
     },
     toggleBioVisible: function(person) {
       // sets bioVisible equal to the opposite of what it was
